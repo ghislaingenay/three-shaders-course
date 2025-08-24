fd42c3d22359a985128a1d49779e579d417619ed
ghislaingenay <ghislain.gny@gmail.com>
2025-08-24 20:39:16 +0700

    feat: add French flag texture and update shaders for wave effects

--- src/shaders/shader/fragment.glsl
+++ src/shaders/shader/fragment.glsl
@@ -1,10 +1,18 @@
-precision mediump float;
+// precision mediump float;
 // how precise can be a float
 // highp / mediump / lowp
 // varying float vRandom;
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+
 
 // highp => high performance but might not work on some device
 // lowp => can create bug by lack of precision
 void main() {
-  gl_FragColor = vec4(1, 0, 0, 1.0);
+  vec4 textureColor = texture2D(uTexture, vUv); // texture2d returns vec4
+  textureColor.rgb *= vElevation * 2.0 + 0.5; // make the waves more visible
+  gl_FragColor = textureColor;
 }
--- src/shaders/shader/vertex.glsl
+++ src/shaders/shader/vertex.glsl
@@ -1,13 +1,19 @@
 // mat4 Each matrix will transform the 'position' until we get the final clip space coordinates
-uniform mat4 projectionMatrix; // transform the coordinates into the clip spaca coordiantes
-uniform mat4 viewMatrix; // apply transformations relative to the camera
-uniform mat4 modelMatrix; // apply transformations relative to the Mesh (position, rotation, scale)
-attribute vec3 position; // Get from geometry directly => BufferGeometry
+// uniform mat4 projectionMatrix; // transform the coordinates into the clip spaca coordiantes
+// uniform mat4 viewMatrix; // apply transformations relative to the camera
+// uniform mat4 modelMatrix; // apply transformations relative to the Mesh (position, rotation, scale)
+// attribute vec3 position; // Get from geometry directly => BufferGeometry
 
+uniform vec2 uFrequency;
+uniform float uTime;
 attribute float aRandom; // take from shader.ts
+uniform vec3 uColor; // take from shader.ts
 // can combine view and modelMatrix with modelViewMatrix
 // uniform mat4 modelViewMatrix;
 
+// attribute vec2 uv; // no need to use a becaause attrib ute already exisst in the material
+varying vec2 vUv;
+varying float vElevation;
 // To apply a matrix, we multiply it
 
 // varying float vRandom;
@@ -16,15 +22,20 @@ void main() {
   // Add the vertex positions to the render
   vec4 modelPosition = modelMatrix * vec4(position, 1.0); 
 
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  vElevation = elevation;
   // Separate the position to get the waves
-  // modelPosition.z += sin(modelPosition.x * 20.0) * 0.1; // 20: spatial angualr frequency (k) - 0.1: Amplitude
-  // modelPosition.z += aRandom * 0.1;
+  modelPosition.z += elevation;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
   gl_Position = projectedPosition;
 
+
   // vRandom = aRandom;
+  vUv = uv;
 }
 
 // The w component encodes perspective.
