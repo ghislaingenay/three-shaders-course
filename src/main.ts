a932d526ae456c36a23e6da2471146e8dadc92a1
ghislaingenay <ghislain.gny@gmail.com>
2025-08-27 20:24:34 +0700

    feat: add hologram shader with vertex and fragment shaders

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ function launch(file: string) {
   });
 }
 
-launch("coffee_smoke");
+launch("hologram");
--- src/shaders/hologram/vertex.glsl
+++ src/shaders/hologram/vertex.glsl
@@ -1,9 +1,14 @@
+varying vec2 vUv;
+
+#include ./rotate2D.glsl
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
+
 }
 
 // void main()
